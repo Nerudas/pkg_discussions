349bf06929a2508e026292c682a003b969ecad27
Septdir <septdir@gmail.com>
2018-05-21 14:07:31 +0300

    add entry point

--- com_discussions/admin/controller.php
+++ com_discussions/admin/controller.php
@@ -10,3 +10,16 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\MVC\Controller\BaseController;
+
+class DiscussionsController extends BaseController
+{
+	/**
+	 * The default view.
+	 *
+	 * @var    string
+	 *
+	 * @since  1.0.0
+	 */
+	protected $default_view = 'posts';
+}
--- com_discussions/admin/helpers/discussions.php
+++ com_discussions/admin/helpers/discussions.php
@@ -10,3 +10,28 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Helper\ContentHelper;
+use Joomla\CMS\Language\Text;
+
+class DiscussionsHelper extends ContentHelper
+{
+	/**
+	 * Configure the Linkbar.
+	 *
+	 * @param   string $vName The name of the active view.
+	 *
+	 * @return  void
+	 *
+	 * @since 1.0.0
+	 */
+	static function addSubmenu($vName)
+	{
+		JHtmlSidebar::addEntry(Text::_('COM_DISCUSSIONS_POSTS'),
+			'index.php?option=com_discussions&view=posts',
+			$vName == 'posts');
+
+		JHtmlSidebar::addEntry(Text::_('COM_DISCUSSIONS_TOPICS'),
+			'index.php?option=com_discussions&view=topics',
+			$vName == 'topics');
+	}
+}
