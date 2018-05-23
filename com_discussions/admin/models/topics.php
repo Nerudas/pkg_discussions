c59d09cc1942350b1a95abb067d22101c814f0ae
Septdir <septdir@gmail.com>
2018-05-23 13:36:21 +0300

    Add topics admin view

--- com_discussions/admin/models/topics.php
+++ com_discussions/admin/models/topics.php
@@ -10,3 +10,353 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\MVC\Model\ListModel;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Helper\TagsHelper;
+
+use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Uri\Uri;
+
+class DiscussionsModelTopics extends ListModel
+{
+	/**
+	 * Category tags
+	 *
+	 * @var    array
+	 * @since  1.0.0
+	 */
+	protected $_categoryTags = null;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param   array $config An optional associative array of configuration settings.
+	 *
+	 * @since  1.0.0
+	 */
+	public function __construct($config = array())
+	{
+		if (empty($config['filter_fields']))
+		{
+			$config['filter_fields'] = array(
+				'id', 't.id',
+				'context', 't.context',
+				'item_id', 't.item_id',
+				'title', 't.title',
+				'text', 't.text',
+				'images', 't.images',
+				'state', 't.state',
+				'created', 't.created',
+				'created_by', 't.created_by',
+				'attribs', 't.attribs',
+				'metakey', 't.metakey',
+				'metadesc',
+				'access', 't.access',
+				'hits', 't.hits',
+				'region', 't.region',
+				'metadata', 't.metadata',
+				'tags_search', 't.tags_search',
+				'tags_map', 't.tags_map',
+			);
+		}
+		parent::__construct($config);
+	}
+
+
+	/**
+	 * Method to auto-populate the model state.
+	 *
+	 * Note. Calling getState in this method will result in recursion.
+	 *
+	 * @param   string $ordering  An optional ordering field.
+	 * @param   string $direction An optional direction (asc|desc).
+	 *
+	 * @return  void
+	 *
+	 * @since  1.0.0
+	 */
+	protected function populateState($ordering = null, $direction = null)
+	{
+		$search = $this->getUserStateFromRequest($this->context . '.filter.search', 'filter_search');
+		$this->setState('filter.search', $search);
+
+		$published = $this->getUserStateFromRequest($this->context . '.filter.published', 'filter_published', '');
+		$this->setState('filter.published', $published);
+
+		$access = $this->getUserStateFromRequest($this->context . '.filter.access', 'filter_access');
+		$this->setState('filter.access', $access);
+
+		$created_by = $this->getUserStateFromRequest($this->context . '.filter.created_by', 'filter_created_by');
+		$this->setState('filter.created_by', $created_by);
+
+		$region = $this->getUserStateFromRequest($this->context . '.filter.region', 'filter_region', '');
+		$this->setState('filter.region', $region);
+
+		$category = $this->getUserStateFromRequest($this->context . '.filter.category', 'filter_category', '');
+		$this->setState('filter.category', $category);
+
+		// List state information.
+		$ordering  = empty($ordering) ? 't.created' : $ordering;
+		$direction = empty($direction) ? 'desc' : $direction;
+		parent::populateState($ordering, $direction);
+	}
+
+	/**
+	 * Method to get a store id based on model configuration state.
+	 *
+	 * This is necessary because the model is used by the component and
+	 * different modules that might need different sets of data or different
+	 * ordering requirements.
+	 *
+	 * @param   string $id A prefix for the store id.
+	 *
+	 * @return  string  A store id.
+	 *
+	 * @since  1.0.0
+	 */
+	protected function getStoreId($id = '')
+	{
+		$id .= ':' . $this->getState('filter.search');
+		$id .= ':' . $this->getState('filter.access');
+		$id .= ':' . $this->getState('filter.published');
+		$id .= ':' . $this->getState('filter.created_by');
+		$id .= ':' . $this->getState('filter.region');
+		$id .= ':' . $this->getState('filter.category');
+
+		return parent::getStoreId($id);
+	}
+
+
+	/**
+	 * Build an SQL query to load the list data.
+	 *
+	 * @return  JDatabaseQuery
+	 *
+	 * @since  1.0.0
+	 */
+	protected function getListQuery()
+	{
+		$db    = $this->getDbo();
+		$query = $db->getQuery(true)
+			->select('t.*')
+			->from($db->quoteName('#__discussions_topics', 't'));
+
+		// Join over the author.
+		$offline      = (int) ComponentHelper::getParams('com_profiles')->get('offline_time', 5) * 60;
+		$offline_time = Factory::getDate()->toUnix() - $offline;
+		$query->select(array(
+			'author.id as author_id',
+			'author.name as author_name',
+			'author.avatar as author_avatar',
+			'author.status as author_status',
+			'(session.time IS NOT NULL) AS author_online',
+			'(company.id IS NOT NULL) AS author_job',
+			'company.id as author_job_id',
+			'company.name as author_job_name',
+			'company.logo as author_job_logo',
+			'employees.position as  author_position'
+		))
+			->join('LEFT', '#__profiles AS author ON author.id = t.created_by')
+			->join('LEFT', '#__session AS session ON session.userid = author.id AND session.time > ' . $offline_time)
+			->join('LEFT', '#__companies_employees AS employees ON employees.user_id = author.id AND ' .
+				$db->quoteName('employees.key') . ' = ' . $db->quote(''))
+			->join('LEFT', '#__companies AS company ON company.id = employees.company_id AND company.state = 1');
+
+		// Join over the asset groups.
+		$query->select('ag.title AS access_level')
+			->join('LEFT', '#__viewlevels AS ag ON ag.id = t.access');
+
+		// Join over the regions.
+		$query->select(array('r.id as region_id', 'r.name AS region_name'))
+			->join('LEFT', '#__regions AS r ON r.id = 
+					(CASE t.region WHEN ' . $db->quote('*') . ' THEN 100 ELSE t.region END)');
+
+
+		// Filter by access level.
+		$access = $this->getState('filter.access');
+		if (is_numeric($access))
+		{
+			$query->where('t.access = ' . (int) $access);
+		}
+
+		// Filter by regions
+		$region = $this->getState('filter.region');
+		if (is_numeric($region))
+		{
+			JModelLegacy::addIncludePath(JPATH_SITE . '/components/com_nerudas/models');
+			$regionModel = JModelLegacy::getInstance('regions', 'NerudasModel');
+			$regions     = $regionModel->getRegionsIds($region);
+			$regions[]   = $db->quote('*');
+			$regions[]   = $regionModel->getRegion($region)->parent;
+			$regions     = array_unique($regions);
+			$query->where($db->quoteName('t.region') . ' IN (' . implode(',', $regions) . ')');
+		}
+
+		// Filter by published state
+		$published = $this->getState('filter.published');
+
+		if (is_numeric($published))
+		{
+			$query->where('t.state = ' . (int) $published);
+		}
+		elseif ($published === '')
+		{
+			$query->where('(t.state = 0 OR t.state = 1)');
+		}
+
+		// Filter by created_by
+		$created_by = $this->getState('filter.created_by');
+		if (!empty($created_by))
+		{
+			$query->where('t.created_by = ' . (int) $created_by);
+		}
+
+		// Filter by category
+		$category = $this->getState('filter.category');
+		if ($category > 1 || $category == 'without')
+		{
+			$categoryTags = $this->getCategoryTags($category);
+
+			$sql = array();
+			foreach ($categoryTags as $tags)
+			{
+				if (!empty($tags))
+				{
+					$categorySql = array();
+					$operator    = ($category != 'without') ? ' LIKE ' : ' NOT LIKE ';
+					foreach ($tags as $tag)
+					{
+
+						$categorySql[] = $db->quoteName('t.tags_map') . $operator . $db->quote('%[' . $tag . ']%');
+					}
+				}
+				$operator = ($category != 'without') ? ' AND ' : ' OR ';
+				$sql[]    = '(' . implode($operator, $categorySql) . ')';
+			}
+
+			if (!empty($sql))
+			{
+				$operator = ($category != 'without') ? ' OR ' : ' AND ';
+				$query->where('(' . implode($operator, $sql) . ')');
+			}
+		}
+
+		// Filter by search.
+		$search = $this->getState('filter.search');
+		if (!empty($search))
+		{
+			if (stripos($search, 'id:') === 0)
+			{
+				$query->where('t.id = ' . (int) substr($search, 3));
+			}
+			else
+			{
+				$cols = array('t.title', 'r.name', 't.text', 't.tags_search', 'ua.name');
+				$sql  = array();
+				foreach ($cols as $col)
+				{
+					$sql[] = $db->quoteName($col) . ' LIKE '
+						. $db->quote('%' . str_replace(' ', '%', $db->escape(trim($search), true) . '%'));
+				}
+				$query->where('(' . implode(' OR ', $sql) . ')');
+			}
+		}
+
+		// Group by
+		$query->group(array('t.id'));
+
+		// Add the list ordering clause.
+		$ordering  = $this->state->get('list.ordering', 't.created');
+		$direction = $this->state->get('list.direction', 'desc');
+		$query->order($db->escape($ordering) . ' ' . $db->escape($direction));
+
+		return $query;
+	}
+
+	/**
+	 * Method to get an array of data items.
+	 *
+	 * @return  mixed  An array of data items on success, false on failure.
+	 *
+	 * @since  1.0.0
+	 */
+	public function getItems()
+	{
+		$items = parent::getItems();
+		if (!empty($items))
+		{
+			foreach ($items as &$item)
+			{
+				$author_avatar       = (!empty($item->author_avatar) && JFile::exists(JPATH_ROOT . '/' . $item->author_avatar)) ?
+					$item->author_avatar : 'media/com_profiles/images/no-avatar.jpg';
+				$item->author_avatar = Uri::root(true) . '/' . $author_avatar;
+
+				$item->author_job_logo = (!empty($item->author_job_logo) && JFile::exists(JPATH_ROOT . '/' . $item->author_job_logo)) ?
+					Uri::root(true) . '/' . $item->author_job_logo : false;
+
+				// Get Tags
+				$item->tags = new TagsHelper;
+				$item->tags->getItemTags('com_discussions.topic', $item->id);
+			}
+		}
+
+		return $items;
+	}
+
+	/**
+	 * Method to get an array of category tags.
+	 *
+	 * @param int $pk category id
+	 *
+	 * @return  mixed  An array of data items on success, false on failure.
+	 *
+	 * @since  1.0.0
+	 */
+
+	/**
+	 * Method to get an array of category tags.
+	 *
+	 * @param int $pk category id
+	 *
+	 * @return  mixed  An array of data items on success, false on failure.
+	 *
+	 * @since  1.0.0
+	 */
+	public function getCategoryTags($pk = null)
+	{
+		$pk = (!empty($pk)) ? $pk : $this->getState('filter.category');
+		if (!isset($this->_categoryTags[$pk]))
+		{
+			try
+			{
+				$tags = array();
+				if (!empty($pk))
+				{
+					$db    = Factory::getDbo();
+					$query = $db->getQuery(true)
+						->select(array('c.id', 'c.items_tags'))
+						->from($db->quoteName('#__discussions_categories', 'c'))
+						->join('LEFT', '#__board_categories as this ON c.lft > this.lft AND c.rgt < this.rgt')
+						->where('(this.id = ' . (int) $pk . ' OR c.id = ' . $pk . ')');
+					$db->setQuery($query);
+					$categories = $db->loadObjectList();
+
+					foreach ($categories as $category)
+					{
+						$tags[$category->id] = array_unique(explode(',', $category->items_tags));
+					}
+
+				}
+				$this->_categoryTags[$pk] = $tags;
+			}
+			catch (Exception $e)
+			{
+				$this->setError($e);
+				$this->_categoryTags[$pk] = false;
+			}
+		}
+
+		return $this->_categoryTags[$pk];
+	}
+
+}
--- com_discussions/admin/views/topics/tmpl/default.php
+++ com_discussions/admin/views/topics/tmpl/default.php
@@ -10,3 +10,183 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Layout\LayoutHelper;
+
+HTMLHelper::_('bootstrap.tooltip');
+HTMLHelper::_('behavior.multiselect');
+HTMLHelper::_('formbehavior.chosen', 'select');
+HTMLHelper::stylesheet('media/com_topics/css/admin-topics.min.css', array('version' => 'auto'));
+
+$app       = Factory::getApplication();
+$doc       = Factory::getDocument();
+$user      = Factory::getUser();
+$userId    = $user->get('id');
+$listOrder = $this->escape($this->state->get('list.ordering'));
+$listDirn  = $this->escape($this->state->get('list.direction'));
+
+$columns = 6;
+
+?>
+
+<form action="<?php echo Route::_('index.php?option=com_discussions&view=topics'); ?>" method="post" name="adminForm"
+      id="adminForm">
+	<div id="j-sidebar-container" class="span2">
+		<?php echo $this->sidebar; ?>
+	</div>
+	<div id="j-main-container" class="span10">
+		<?php echo LayoutHelper::render('joomla.searchtools.default',
+			array('view' => $this, 'options' => array('filtersHidden' => false))); ?>
+		<?php if (empty($this->items)) : ?>
+			<div class="alert alert-no-items">
+				<?php echo Text::_('JGLOBAL_NO_MATCHING_RESULTS'); ?>
+			</div>
+		<?php else : ?>
+			<table id="itemList" class="table table-striped">
+				<thead>
+				<tr>
+					<th width="1%" class="center">
+						<?php echo HTMLHelper::_('grid.checkall'); ?>
+					</th>
+					<th width="2%" style="min-width:100px" class="center">
+						<?php echo HTMLHelper::_('searchtools.sort', 'JSTATUS', 't.state', $listDirn, $listOrder); ?>
+					</th>
+					<th style="min-width:100px" class="nowrap">
+						<?php echo HTMLHelper::_('searchtools.sort', 'JGLOBAL_TITLE', 't.title', $listDirn, $listOrder); ?>
+					</th>
+					<th width="10%" class="nowrap hidden-phone">
+						<?php echo HTMLHelper::_('searchtools.sort', 'JGRID_HEADING_ACCESS', 't.access', $listDirn, $listOrder); ?>
+					</th>
+					<th width="10%" class="nowrap hidden-phone">
+						<?php echo HTMLHelper::_('searchtools.sort', 'JAUTHOR', 't.created_by', $listDirn, $listOrder); ?>
+					</th>
+					<th width="10%" class="nowrap hidden-phone">
+						<?php echo HTMLHelper::_('searchtools.sort', 'JGRID_HEADING_REGION', 'region_name', $listDirn, $listOrder); ?>
+					</th>
+					<th width="10%" class="nowrap hidden-phone">
+						<?php echo HTMLHelper::_('searchtools.sort', 'JGLOBAL_CREATED_DATE', 't.created', $listDirn, $listOrder); ?>
+					</th>
+					<th width="1%" class="nowrap hidden-phone">
+						<?php echo HTMLHelper::_('searchtools.sort', 'JGLOBAL_HITS', 't.hits', $listDirn, $listOrder); ?>
+					</th>
+					<th width="1%" class="nowrap hidden-phone center">
+						<?php echo HTMLHelper::_('searchtools.sort', 'JGRID_HEADING_ID', 't.id', $listDirn, $listOrder); ?>
+					</th>
+				</tr>
+				</thead>
+				<tfoot>
+				<tr>
+					<td colspan="<?php echo $columns; ?>">
+						<?php echo $this->pagination->getListFooter(); ?>
+					</td>
+				</tr>
+				</tfoot>
+				<tbody>
+
+				<?php foreach ($this->items as $i => $item) :
+					$canEdit = $user->authorise('core.edit', '#_discussions_topics.' . $item->id);
+					$canChange = $user->authorise('core.edit.state', '#__discussions_topics' . $item->id);
+					?>
+					<tr item-id="<?php echo $item->id ?>">
+						<td class="center">
+							<?php echo HTMLHelper::_('grid.id', $i, $item->id); ?>
+						</td>
+						<td class="center">
+							<div class="btn-group">
+								<a class="btn btn-micro hasTooltip" title="<?php echo Text::_('JACTION_EDIT'); ?>"
+								   href="<?php echo Route::_('index.php?option=com_discussions&task=topic.edit&id=' . $item->id); ?>">
+									<span class="icon-apply icon-white"></span>
+								</a>
+								<?php echo HTMLHelper::_('jgrid.published', $item->state, $i, 'topics.', $canChange, 'cb'); ?>
+								<?php
+								if ($canChange)
+								{
+									HTMLHelper::_('actionsdropdown.' . ((int) $item->state === -2 ? 'un' : '') . 'trash', 'cb' . $i, 'topics');
+									echo HTMLHelper::_('actionsdropdown.render', $this->escape($item->title));
+								}
+								?>
+							</div>
+						</td>
+						<td>
+							<div>
+								<?php if ($canEdit) : ?>
+									<a class="hasTooltip" title="<?php echo Text::_('JACTION_EDIT'); ?>"
+									   href="<?php echo Route::_('index.php?option=com_discussions&task=topic.edit&id=' . $item->id); ?>">
+										<?php echo $this->escape($item->title); ?>
+									</a>
+								<?php else : ?>
+									<?php echo $this->escape($item->title); ?>
+								<?php endif; ?>
+							</div>
+							<div class="tags">
+								<?php if (!empty($item->tags->itemTags)): ?>
+									<?php foreach ($item->tags->itemTags as $tag): ?>
+										<span class="label label-inverse">
+											<?php echo $tag->title; ?>
+										</span>
+									<?php endforeach; ?>
+								<?php endif; ?>
+							</div>
+						</td>
+						<td class="small hidden-phone">
+							<?php echo $this->escape($item->access_level); ?>
+						</td>
+						<td class="hidden-phone">
+							<?php if ((int) $item->created_by != 0) : ?>
+								<div class="author">
+									<div class="avatar<?php echo ($item->author_online) ? ' online' : '' ?>"
+									     style="background-image: url('<?php echo $item->author_avatar; ?>')">
+									</div>
+									<div>
+										<div class="name">
+											<a class="hasTooltip nowrap" title="<?php echo Text::_('JACTION_EDIT'); ?>"
+											   target="_blank"
+											   href="<?php echo Route::_('index.php?option=com_users&task=user.edit&id='
+												   . (int) $item->author_id); ?>">
+												<?php echo $item->author_name; ?>
+											</a>
+										</div>
+										<?php if ($item->author_job): ?>
+											<div class="job">
+												<a class="hasTooltip nowrap"
+												   title="<?php echo Text::_('JACTION_EDIT'); ?>"
+												   target="_blank"
+												   href="<?php echo Route::_('index.php?option=com_companies&task=company.edit&id='
+													   . $item->author_job_id); ?>">
+													<?php echo $this->escape($item->author_job_name); ?>
+												</a>
+											</div>
+										<?php endif; ?>
+									</div>
+								</div>
+							<?php endif; ?>
+						</td>
+						<td class="small hidden-phone nowrap">
+							<?php echo ($item->region !== '*') ? $this->escape($item->region_name) :
+								Text::_('JGLOBAL_FIELD_REGIONS_ALL'); ?>
+						</td>
+						<td class="nowrap small hidden-phone">
+							<?php echo $item->created > 0 ? HTMLHelper::_('date', $item->created,
+								Text::_('DATE_FORMAT_LC2')) : '-' ?>
+						</td>
+						<td class="hidden-phone center">
+							<span class="badge badge-info">
+								<?php echo (int) $item->hits; ?>
+							</span>
+						</td>
+						<td class="hidden-phone center">
+							<?php echo $item->id; ?>
+						</td>
+					</tr>
+				<?php endforeach; ?>
+				</tbody>
+			</table>
+		<?php endif; ?>
+		<input type="hidden" name="task" value=""/>
+		<input type="hidden" name="boxchecked" value="0"/>
+		<?php echo HTMLHelper::_('form.token'); ?>
+	</div>
+</form>
